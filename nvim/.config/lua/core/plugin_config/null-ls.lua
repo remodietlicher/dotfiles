96ba85f133aebf06df1e0a4fe88dd3abd2127f8f
Remo Dietlicher <remo.d@gmx.ch>
2022-12-24 23:01:44 +0100

    add formatters and keymap for auto-format

--- nvim/.config/lua/core/plugin_config/null-ls.lua
+++ nvim/.config/lua/core/plugin_config/null-ls.lua
@@ -3,7 +3,12 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.black,
 		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.diagnostics.flake8,
 		null_ls.builtins.completion.spell,
 	},
 })
+
+vim.keymap.set("n", "<leader>af", vim.lsp.buf.format, { desc = "[A]pply [F]ormat" })
