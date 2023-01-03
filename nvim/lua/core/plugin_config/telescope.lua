48e26e1e6f27e720c863d61c21c569741653b2d5
Remo Dietlicher <remo.d@gmx.ch>
2023-01-03 13:35:20 +0100

    more intuitive telescope mapping and improved descripions

--- nvim/lua/core/plugin_config/telescope.lua
+++ nvim/lua/core/plugin_config/telescope.lua
@@ -3,10 +3,10 @@ require("telescope").load_extension("fzf")
 
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fs", function()
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]ile" })
+vim.keymap.set("n", "<leader>fw", function()
   builtin.grep_string({ search = "", only_sort_text = true })
-end, {})
+end, { desc = "[F]ind [W]ord" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [B]uffer" })
 vim.keymap.set("n", "<leader>/", function()
