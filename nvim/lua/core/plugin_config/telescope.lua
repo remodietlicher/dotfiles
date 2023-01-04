2fe86ecffe2fa1a0933cae610f9c520d373f2fd3
Remo Dietlicher <remo.d@gmx.ch>
2023-01-04 20:44:27 +0100

    sort recent buffers by last used

--- nvim/lua/core/plugin_config/telescope.lua
+++ nvim/lua/core/plugin_config/telescope.lua
@@ -8,7 +8,9 @@ vim.keymap.set("n", "<leader>fw", function()
   builtin.grep_string({ search = "", only_sort_text = true })
 end, { desc = "[F]ind [W]ord" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [B]uffer" })
+vim.keymap.set("n", "<leader>fb", function()
+  builtin.buffers({ sort_mru = true })
+end, { desc = "[F]ind [B]uffer" })
 vim.keymap.set("n", "<leader>/", function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
