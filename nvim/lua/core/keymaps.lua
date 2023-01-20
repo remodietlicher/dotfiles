8f75207aae100aad9af9629350be52d12fa6ac77
Remo Dietlicher <remo.d@gmx.ch>
2023-01-20 22:18:01 +0100

    handy shortcuts to yank filename and split panes

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -25,8 +25,8 @@ vim.o.smartcase = true
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "No search highlight" })
 
 -- pane management
-vim.keymap.set("n", "<c-L>", ":vsplit<CR>", { desc = "Split pane vertically" })
-vim.keymap.set("n", "<c-K>", ":split<CR>", { desc = "Split pane horizontally" })
+vim.keymap.set("n", "<leader>sl", ":vsplit<CR>", { desc = "Split pane vertically" })
+vim.keymap.set("n", "<leader>sk", ":split<CR>", { desc = "Split pane horizontally" })
 vim.keymap.set("n", "<c-h>", "<c-w>h", { desc = "Move pane left" })
 vim.keymap.set("n", "<c-j>", "<c-w>j", { desc = "Move pane down" })
 vim.keymap.set("n", "<c-k>", "<c-w>k", { desc = "Move pane up" })
@@ -35,10 +35,32 @@ vim.keymap.set("n", "<c-l>", "<c-w>l", { desc = "Move pane right" })
 -- navigation
 vim.keymap.set("n", "<a-left>", "<c-O>", { desc = "Go back" })
 vim.keymap.set("n", "<a-right>", "<c-I>", { desc = "Go forward" })
+vim.keymap.set("n", "<a-k>", ":bp<CR>", { desc = "Open previous buffer" })
+vim.keymap.set("n", "<a-j>", ":bn<CR>", { desc = "Open next buffer" })
 
 -- useful shortcuts
 vim.keymap.set("v", "<leader>p", '"_dp', { desc = "Put text from visual selection into _ registry" })
 vim.keymap.set("v", "<c-c>", '"+y', { desc = "Yank to system clipboard" })
+
+-- yanking filename (very useful for markdown links!)
+vim.keymap.set(
+  "n",
+  "<leader>yr",
+  ":let @+ = expand('%')<CR>",
+  { desc = "[Y]ank [R]elative file name to unnamed register" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>ya",
+  ":let @+ = expand('%:p')<CR>",
+  { desc = "[Y]ank [A]bsolute file name to unnamed register" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>yf",
+  ":let @+ = expand('%:t')<CR>",
+  { desc = "[Y]ank [F]ile name to unnamed register" }
+)
 
 -- toggle normal mode in terminal mode
 vim.keymap.set("t", "<c-n>", "<c-\\><c-n>", { desc = "Enter [N]ormal mode in terminal" })
