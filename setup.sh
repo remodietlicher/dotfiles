72ffa78d07a630ac1f7c05bf88ee14aba0f774b5
Remo Dietlicher <remo.d@gmx.ch>
2023-02-24 23:10:43 +0100

    add vim configuration for intellij IDEs

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal/dist/rose-pine-moon.dconf
+DOTFILE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+ln -s $DOTFILE_DIR/nvim $HOME/.config/nvim
+ln -s $DOTFILE_DIR/alacritty $HOME/.config/alacritty
+
+ln -s $DOTFILE_DIR/tmuxp $HOME/.tmuxp
+ln -s $DOTFILE_DIR/tmux/tmux.conf $HOME/.tmux.conf
+
+ln -s $DOTFILE_DIR/ideavim/.ideavimrc $HOME/.ideavimrc
+
