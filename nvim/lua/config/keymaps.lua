d5b5b5d3c8f624a85aa92df3d6a726b0608414bc
Remo Dietlicher <dietlicher@ubique.ch>
2026-06-13 10:54:02 +0200

    set some basic config maps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Format the current document (conform + LSP fallback, via LazyVim).
+vim.keymap.set({ "n", "v" }, "<leader>af", function()
+  LazyVim.format({ force = true })
+end, { desc = "Apply format (document)" })
+
+-- Find word in files (live grep). Find files (<leader>ff) and buffers (<leader>fb) are LazyVim defaults.
+vim.keymap.set("n", "<leader>fw", "<cmd>FzfLua live_grep<cr>", { desc = "Find word in files" })
